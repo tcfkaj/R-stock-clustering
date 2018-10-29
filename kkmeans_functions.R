bcbac2401e15b13bf5890cb98857757855659efe
tcfkaj <mr.jared.turner@gmail.com>
2018-10-29 12:51:35 -0500

    feature/in_H

--- kkmeans_functions.R
+++ kkmeans_functions.R
@@ -1,6 +1,190 @@
 # Defines functions for optimizing kkmeans
 
 library(kernlab)
+library(tidyverse)
+
+## Compute the following in Hilbert space
+
+# Computes the Norm - distance squared - in Hilbert space
+distance_from_center <- function(x, clusters, kernelMatrix){
+		cluster=which(clusters==clusters[x]);
+		r <- length(cluster);
+		k.x.x <- kernelMatrix[x,x]
+		part2 <- 0;
+		part3 <- 0;
+	for (i in cluster){
+		for (j in cluster){
+				part2 <- part2+kernelMatrix[i,j]
+			}
+	part3 <- part3+kernelMatrix[x,i]
+	}
+	part2 <- part2/r^2;
+	part3 <- part3*(-2/r);
+	distance <- k.x.x + part2 + part3
+	return(distance)
+}
+
+radii <- function(nclusters, clusters, kernelMatrix){
+	radii <- c();
+	for (i in 1:nclusters){
+		current_cluster <- which(clusters==i)
+		distances <- sapply(current_cluster,
+						distance_from_center,
+						clusters=clusters,
+						kernelMatrix=kernelMatrix);
+		print(distances)
+		radii <- c(radii, unname(quantile(distances, 0.95)))
+		}
+	return(radii)
+}
+
+## Norm squared between cluster centers i,j  in Hilbert space
+nsbcc <- function(i, j, clusters, kernelMatrix){
+		clusteri <- which(clusters==i);
+		clusterj <- which(clusters==j);
+		a <- length(clusteri);
+		b <- length(clusterj);
+		X = Y = Z = 0;
+		for (m1 in clusteri){
+			for (m2 in clusteri){
+				X <- X+kernelMatrix[m1,m2]
+				}
+			for (n1 in clusterj){
+				Y <- Y+kernelMatrix[m1,n1]
+				}
+		}
+		for (n1 in clusterj){
+			for(n2 in clusterj){
+				Z <- Z+kernelMatrix[n1,n2]
+			}
+		}
+		ns <- (X/a^2)-(2*Y/(a*b))+(Z/b^2);
+		return(ns)
+}
+
+## Compute the seperability matrix
+
+smat <- function(nclusters, clusters, kernelMatrix){
+			smat <- diag(nclusters);
+			radii <- radii(nclusters, clusters, kernelMatrix);
+			for (i in 1:nclusters){
+				for (j in 1:nclusters){
+					nsbcc <- nsbcc(i=i,j=j,
+							clusters=clusters,
+							kernelMatrix=kernelMatrix);
+					sep <- (radii[i]+radii[j])/nsbcc;
+					smat[i,j] <- sep;
+				}
+			}
+			return(smat)
+}
+
+
+disp_sq <- function(cluster_number, clusters, kernelMatrix){
+			cluster <- which(clusters==cluster_number);
+			distances <- sapply(cluster,
+						distance_from_center,
+						clusters=clusters,
+						kernelMatrix=kernelMatrix);
+			disp.sq <- sum(distances)/length(cluster);
+			return(disp.sq)
+}
+
+cost_in_H <- function(nclusters, clusters, kernelMatrix){
+			disp.sq <- c(1:nclusters) %>%
+				sapply(disp_sq, clusters=clusters,
+					kernelMatrix=kernelMatrix) %>%
+				sum();
+			return(disp.sq)
+}
+
+
+best_run_H  <- function(x, kernel, ncenters, nruns){
+		kernelM <- kernelMatrix(x, kernel=kernel);
+		best <- kkmeans(x, kernel=kernel, centers=ncenters);
+		cost_of_best <- cost_in_H(ncenters, best@.Data, kernelM);
+		for (i in 1:(nruns-1)){
+			current <- kkmeans(x, kernel=kernel,
+					centers=ncenters);
+			cost_of_current <- cost_in_H(ncenters, current@.Data, kernelM);
+		if(cost_of_current < cost_of_best){
+			best <- current;
+			cost_of_best <- cost_of_current
+		}
+	}
+	return(best)
+}
+
+# best_offset_poly <- function(x, degree, centers, nruns, min.offset, max.offset){
+#                         best <- best_run(x, kernel=polydot(degree=degree,
+#                                                         offset=min.offset),
+#                                         centers=centers,
+#                                         nruns=nruns);
+#                         cost_of_best <- compute_cost(best);
+#                         min.offset <- min.offset+1;
+#                         while (min.offset <= max.offset){
+#                         current <- best_run(x, kernel=polydot(degree=degree,
+#                                                         offset=min.offset),
+#                                         centers=centers,
+#                                         nruns=nruns);
+#                         cost_of_current <- compute_cost(best);
+#                         if(cost_of_current < cost_of_best){
+#                                 best <- current;
+#                                 cost_of_best <- cost_of_current
+#                                 }
+#                         min.offset <- min.offset+1
+#                         }
+#                         return(best)
+#                 }
+#
+# best_scale_poly <- function(x, degree, centers, nruns, offset, min.scale, max.scale){
+#                         best <- best_run(x, kernel=polydot(degree=degree,
+#                                                         offset=offset,
+#                                                         scale=min.scale),
+#                                         centers=centers,
+#                                         nruns=nruns);
+#                         cost_of_best <- compute_cost(best);
+#                         min.scale <- min.scale+1;
+#                         while (min.scale <= max.scale){
+#                         current <- best_run(x, kernel=polydot(degree=degree,
+#                                                         offset=offset,
+#                                                         scale=min.scale),
+#                                         centers=centers,
+#                                         nruns=nruns);
+#                         cost_of_current <- compute_cost(best);
+#                         if(cost_of_current < cost_of_best){
+#                                 best <- current;
+#                                 cost_of_best <- cost_of_current
+#                                 }
+#                         min.scale <- min.scale+1
+#                         }
+#                         return(best)
+#                 }
+#
+# best_sigma_rfb <- function(x, centers, nruns, min.sigma, max.sigma){
+#                         best <- best_run(x, kernel=rfbdot(sigma=min.sigma),
+#                                         centers=centers,
+#                                         nruns=nruns);
+#                         cost_of_best <- compute_cost(best);
+#                         min.sigma <- min.sigma+1;
+#                         while (min.sigma <= max.sigma){
+#                         current <- best_run(x, kernel=rfbdot(sigma=max.sigma),
+#                                         centers=centers,
+#                                         nruns=nruns);
+#                         cost_of_current <- compute_cost(best);
+#                         if(cost_of_current < cost_of_best){
+#                                 best <- current;
+#                                 cost_of_best <- cost_of_current
+#                                 }
+#                         min.sigma <- min.sigma+1
+#                         }
+#                         return(best)
+#                 }
+#
+
+
+
+## The following compute things in feature space
 
 compute_cost <- function(kkmeans_results_){
 		cost <- sum(withinss(kkmeans_results_)/size(kkmeans_results_));
@@ -87,3 +271,4 @@ best_sigma_rfb <- function(x, centers, nruns, min.sigma, max.sigma){
 			}
 			return(best)
 		}
+
--- test.R
+++ test.R
@@ -1,4 +1,4 @@
-source("kmeans_functions.R")
+source("kkmeans_functions.R")
 library(tidyverse)
 library(kernlab)
 
@@ -23,31 +23,60 @@ test.data <- data.frame(X=x, Y=y, grp=grp)
 
 ## kernel k-means
 
+
 dd <- as.matrix(test.data[,-3])
 dd
 dd.kk <- kkmeans(dd, kernel=polydot(degree=3, offset=-5, scale=1), centers=2)
 dd.kk
-
+compute_cost(dd.kk)
+clusters <- dd.kk@.Data
+kernelM <- kernelMatrix(dd, kernel=polydot(degree=3, offset=-5, scale=1))
+cost_in_H(2, clusters, kernelM)
+smat <- smat(nclusters=2, clusters=clusters, kernelMatrix=kernelM)
+# best <- best_run_H(dd, kernel=polydot(degree=3, offset=-5, scale=1), ncenters=2, nruns=100)
+# best
+# cost_in_H(2, best@.Data, kernelM)
+# kernelf(dd.kk)
+# length(size(dd.kk))
 ## playing around with kkmeans
 
 # str(dd.kk)
-size(dd.kk)[1]
-size(dd.kk)[2]
-withinss(dd.kk)[1]
-withinss(dd.kk)[2]
-disp <- withinss(dd.kk)/size(dd.kk)
-disp
-sum(disp)
-compute_cost(dd.kk)
+# size(dd.kk)[1]
+# size(dd.kk)[2]
+# withinss(dd.kk)[1]
+# withinss(dd.kk)[2]
+# disp <- withinss(dd.kk)/size(dd.kk)
+# disp
+# sum(disp)
+# compute_cost(dd.kk)
+#  dd.kk.best <- best_run(dd, kernel=polydot(degree=3, offset=-5, scale=1), centers=2, nruns=100)
 
+# clusters <- dd.kk@.Data
+# kernelM <- kernelMatrix(dd, kernel=rbfdot(1))
+# distance_from_center(64, clusters, kernelM)
+#
+# current1 <- which(clusters==1)
+# current2 <- which(clusters==2)
+# distances1 <- sapply(current1, distance_from_center, clusters=clusters, kernelMatrix=kernelM)
+# distances2 <- sapply(current2, distance_from_center, clusters=clusters, kernelMatrix=kernelM)
+# distances1
+# distances2
+# unname(quantile(distances,.95))
+# radii(2,clusters,kernelM)
 
-dd.kk.best <- best_run(dd, kernel=polydot(degree=3, offset=-5, scale=1), centers=2, nruns=100)
-dd.kk.best
-compute_cost(dd.kk.best)
+# cluster <- which(dd.kk@.Data==2)
+# cluster
+# length(cluster)
 
-best.scale <- best_scale_poly(dd, degree=3, centers=2, nruns=100, offset=-5, min.scale=1, max.scale=10)
-best.scale
-compute_cost(best.scale)
+# dd.kk.best
+# compute_cost(dd.kk.best)
+#
+# best.offset <- best_offset_poly(dd, degree=3, centers=2, nruns=100, min.offset=0, max.offset=50)
+# best.offset
+# compute_cost(best.offset)
+# best.scale <- best_scale_poly(dd, degree=3, centers=2, nruns=100, offset=-5, min.scale=1, max.scale=10)
+# best.scale
+# compute_cost(best.scale)
 ## optimizing
 ## optimizing cluster sizes, we want a ratio of 0.6
 
